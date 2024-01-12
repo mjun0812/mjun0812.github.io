0024749c039a341d4a233b98bbcd7f64884d4822
Junya Morioka <mjun@mjunya.com>
2024-01-13 07:03:43 +0900

    fix RSS Feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,4 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export const siteTitle = "MJUN Tech Note";
@@ -6,20 +6,21 @@ export const description = "とある情報系の院生の技術ノートです"
 export const siteUrl = "https://mjunya.com";
 
 export async function GET() {
-    let posts = await getCollection("posts");
-    posts = posts.sort(
-        (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
-    );
+  let posts = await getCollection("posts");
+  posts = posts.sort(
+    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
+  );
 
-    return rss({
-        title: siteTitle,
-        description: description,
-        site: siteUrl,
-        items: posts.map((post) => ({
-            title: post.data.title,
-            pubDate: new Date(post.data.date),
-            link: `/post/${post.slug}/`
-        })),
-        customData: `<language>ja-jp</language>`,
-    });
+  return rss({
+    title: siteTitle,
+    description: description,
+    site: siteUrl,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: new Date(post.data.date),
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>ja-jp</language>`,
+  });
 }
+

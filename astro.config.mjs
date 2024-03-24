ae887c92faa09986dfd0bf97aeb5a5754ffe9196
Junya Morioka <mjun@mjunya.com>
2024-03-25 00:00:52 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import remarkLinkCard from "remark-link-card";
-import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,11 +22,5 @@ export default defineConfig({
       theme: 'one-dark-pro'
     }
   },
-  integrations: [partytown(), icon(), sitemap(
-    {
-      filter: (page) => page !== 'https://mjunya.com/tag*',
-      filter: (page) => page !== 'https://mjunya.com/category*',
-      filter: (page) => page !== 'https://mjunya.com/categories',
-    }
-  )]
+  integrations: [partytown(), icon(), sitemap()]
 });

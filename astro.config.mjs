23804fcec157d42fd5ceea48de0a36ac1afe3f98
Junya Morioka <mjun@mjunya.com>
2025-06-16 01:30:34 +0900

    update gtm

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
     sitemap(),
     partytown({
       config: {
-        forward: ["dataLayer.push"],
+        forward: ["dataLayer.push", "gtm"],
       }
     }),
   ],
